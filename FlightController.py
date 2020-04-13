377148a56b4b5f7210dcfa97fe8dfafbb7b97347
ArneVanDongen <arne.vdongen@gmail.com>
2020-04-13 14:10:48 +0200

    added telemetry logging

--- FlightController.py
+++ FlightController.py
@@ -22,9 +22,24 @@ def log_trajectory_status():
 
 connection = krpc.connect()
 vessel = connection.space_center.active_vessel
-vessel.control.throttle = 1
+vessel.control.throttle = 0.3
 objective_achieved = False
 flight = vessel.flight()
+
+# Streams for telemetry
+ut = connection.add_stream(getattr, connection.space_center, 'ut')
+altitude = connection.add_stream(getattr, vessel.flight(), 'mean_altitude')
+apoapsis = connection.add_stream(getattr, vessel.orbit, 'apoapsis_altitude')
+stage_0_resources = vessel.resources_in_decouple_stage(stage=0, cumulative=False)
+liquid_fuel_stage_0 = connection.add_stream(stage_0_resources.amount, 'LiquidFuel')
+stage_1_resources = vessel.resources_in_decouple_stage(stage=1, cumulative=False)
+liquid_fuel_stage_1 = connection.add_stream(stage_1_resources.amount, 'LiquidFuel')
+stage_2_resources = vessel.resources_in_decouple_stage(stage=2, cumulative=False)
+liquid_fuel_stage_2 = connection.add_stream(stage_2_resources.amount, 'LiquidFuel')
+stage_3_resources = vessel.resources_in_decouple_stage(stage=3, cumulative=False)
+liquid_fuel_stage_3 = connection.add_stream(stage_3_resources.amount, 'LiquidFuel')
+stage_4_resources = vessel.resources_in_decouple_stage(stage=4, cumulative=False)
+liquid_fuel_stage_4 = connection.add_stream(stage_4_resources.amount, 'LiquidFuel')
 
 print('Start lon', flight.longitude)
 print('Start lat', flight.latitude)
@@ -33,6 +48,16 @@ print('Start alt', flight.mean_altitude)
 
 while not objective_achieved:
     print('\nT=', round(vessel.met, 1))
+
+    print('ut?', ut())
+    print('altitude', altitude())
+    print('apoapsis', apoapsis())
+    print('liquid_fuel_stage_0', liquid_fuel_stage_0())
+    print('liquid_fuel_stage_1', liquid_fuel_stage_1())
+    print('liquid_fuel_stage_2', liquid_fuel_stage_2())
+    print('liquid_fuel_stage_3', liquid_fuel_stage_3())
+    print('liquid_fuel_stage_4', liquid_fuel_stage_4())
+
     log_vessel_status()
 
     desired_unit_vector = calc_desired_unit_vector(flight.longitude, flight.mean_altitude)
